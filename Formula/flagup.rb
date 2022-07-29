b1c0865518a6a92fe3b962d0d3d0915411f21a94
Christopher Hogg <christopher@luminovo.ai>
2022-07-29 14:31:12 +0100

    feat: version 2

--- Formula/flagup.rb
+++ Formula/flagup.rb
@@ -5,8 +5,8 @@ class Flagup < Formula
     desc "Cross-platform flag finder written in Rust"
     homepage "https://github.com/cthogg"
     url "https://github.com/cthogg/flagup/releases/latest/download/flagup-mac.tar.gz"
-    sha256 "27a9dc951af99ced7de799eb7113c471770c8124c4b26864ea43fabdb785313d"
-    version "0.0.1"
+    sha256 "94d4ccba263464232adc95f3a1dca4bc990524e309a2fc5919025bdbc9212d9c"
+    version "0.0.2"
   
     def install
       bin.install "flagup"
