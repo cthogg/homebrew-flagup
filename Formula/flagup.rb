28bf897ba3756741c133f69c0ca2cb3dc8b8d851
Christopher Hogg <cthogg@users.noreply.github.com>
2022-08-13 20:36:11 +0100

    Update flagup.rb

--- Formula/flagup.rb
+++ Formula/flagup.rb
@@ -6,7 +6,7 @@ class Flagup < Formula
     homepage "https://github.com/cthogg"
     url "https://github.com/cthogg/flagup/releases/latest/download/flagup-mac.tar.gz"
     sha256 "ea86f3624497f70eaed64ff7be8c1bd408531b44c93e82073216821d2eb39644" # This should change at every release. 
-    version "0.0.5" # This should also change at every release.
+    version "0.0.6" # This should also change at every release.
   
     def install
       bin.install "flagup-cli"
