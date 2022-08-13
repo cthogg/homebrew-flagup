92eccacf327b3cc34a464df8e32ad7ff37029237
Christopher Hogg <cthogg@users.noreply.github.com>
2022-08-13 20:44:53 +0100

    Update flagup.rb

--- Formula/flagup.rb
+++ Formula/flagup.rb
@@ -5,11 +5,11 @@ class Flagup < Formula
     desc "Cross-platform flag finder written in Rust"
     homepage "https://github.com/cthogg"
     url "https://github.com/cthogg/flagup/releases/latest/download/flagup-mac.tar.gz"
-    sha256 "48f207f0630c4e9f6e099fd3760b3cf10cf43962ecbbe1bcef6f14a6e07708d9" # This should change at every release. 
-    version "0.0.7" # This should also change at every release.
+    sha256 "e27e3b138974c49e044af89ca309c063b17ac3c3660f64d6f31a15519b1521e7" # This should change at every release. 
+    version "0.0.8" # This should also change at every release.
   
     def install
-      bin.install "flagup-cli"
+      bin.install "flagup"
     end
   end
   
